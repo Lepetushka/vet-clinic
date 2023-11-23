e6d3266c3d9f79e5606862236a8e86051805a0b1
Nadiia Lepetan <91819083+Lepetushka@users.noreply.github.com>
2023-11-23 16:50:10 +0100

    1 commit

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -1,15 +1,30 @@
-package main.java.com.magicvet;
-
-import main.java.com.magicvet.component.ApplicationRunner;
+package main.java.com.magicvet;//import main.java.com.magicvet.component.ApplicationRunner;
 import java.util.Scanner;
 
 public class Main {
 
+	static String PASSWORD = "default";
+
 	public static Scanner SCANNER = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		ApplicationRunner runner = new ApplicationRunner();
-		runner.run();
+		run();
+	}
+
+	static void run(){
+		boolean assepted = false;
+		for(int i=0; i<3; i++){
+			System.out.print("Password: ");
+			String input = SCANNER.nextLine();
+
+			if(PASSWORD.equals(input)){
+				assepted=true;
+				break;
+			} else {
+				System.out.println("Access denied. Please check your password.");
+			}
+		}
+		System.out.println(assepted ? "Welcome to the Magic Vet!" : "Application has been blocked!");
 	}
 
 }
