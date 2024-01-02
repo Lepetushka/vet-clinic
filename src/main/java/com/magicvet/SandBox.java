c8a96b3c812e767ec559fb14e13c5f18b7d27f89
Nadiia Lepetan <91819083+Lepetushka@users.noreply.github.com>
2024-01-02 16:02:11 +0100

    main

--- src/main/java/com/magicvet/SandBox.java
+++ src/main/java/com/magicvet/SandBox.java
@@ -11,20 +11,7 @@ import java.util.Arrays;
 public class SandBox {
     public static void main(String[] args) {
 
-        Pet[] pets={
-                new Pet(5),
-                new Pet( 8),
 
-                new Pet(1),
-                new Pet(4),
-                new Pet(2),
-                new Pet(1)
 
-        };
-
-        Arrays.sort(pets,new AgeComparator());
-        for(Pet pet:pets){
-            System.out.println(pet.getAge());
-        }
     }
 }
